6ab4b2119a6a822dd036da9616c92cffcc0bf617
Michael <keke231@qq.com>
2015-08-17 22:00:18 +0800

    add predis

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -6,12 +6,17 @@ class HomeController extends BaseController
     {
         //$data = Article::first();
         //include dirname(__FILE__) . '/../views/home.php';
-        $this->view = View::make('home.index')->with('article',Article::first())
-                                        ->withTitle('Hello Hilda');
 
-        $this->mail = Mail::to(['keke231@qq.com', '820086734@qq.com'])
-            ->from('MichaelHan <hanfeng@163.com>')
-            ->title('This email is tested by Hilda.')
-            ->content('<h1>Hello~~</h1>');
+
+//        $this->view = View::make('home.index')->with('article',Article::first())
+//                                        ->withTitle('Hello Hilda');
+
+//        $this->mail = Mail::to(['keke231@qq.com', '820086734@qq.com'])
+//            ->from('MichaelHan <hanfeng@163.com>')
+//            ->title('This email is tested by Hilda.')
+//            ->content('<h1>Hello~~</h1>');
+
+        Redis::set('key','hello hilda',5,'s');
+        echo Redis::get('key');
     }
 }
